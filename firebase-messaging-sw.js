eafd6a1f4ea661395ed0739f75fc1f9da4ae1570
vyacheslav74 <kiferon@bk.ru>
2023-09-06 15:58:59 +0500

    Deploy

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,7 +1,11 @@
-importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js");
-importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js");
+importScripts("https://www.gstatic.com/firebasejs/10.3.0/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/10.3.0/firebase-messaging.js");
 
-firebase.initializeApp({});
+firebase.initializeApp({
+ 
+  
+
+});
 // Necessary to receive background messages:
 const messaging = firebase.messaging();
 
